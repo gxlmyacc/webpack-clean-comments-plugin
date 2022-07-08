690f70127fa3a08205d11242a95058b3b7247bad
gxlmyacc <gxl.mymail@163.com>
2022-07-08 11:46:05 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # webpack-clean-comments-plugin
-clean webpack assets comments
+a webpack plugin that clean webpack assets comments
+
+## installtion
+
+```bash
+  npm install --save-dev webpack-clean-comments-plugin
+  // or 
+  yarn add -D webpack-clean-comments-plugin
+```
+
+## config
+
+
+```js
+const WebpackCleanCommentsPlugin = require('./webpack-clean-comments-plugin');
+
+// babel.config.js
+module.exports = {
+ 
+  plugins: [
+   ...
+   new WebpackCleanCommentsPlugin({
+    comments: [
+      /^\/\*[A-Za-z0-9\s*():/.",\-!_$@#%&~]+\*\/\n?/
+    ]
+   }),
+   ...
+  ]
+};
+```
+## options
+
+- `comments` comment string or RegExp list, default is `[/^\/\*[A-Za-z0-9\s*():/.",\-!_$@#%&~]+\*\/\n?/]`.
+
+- `test` - remove files filter, default is `/\.js($|\?)/i`.
---
+++ src/index.js
@@ -0,0 +1,95 @@
+const { sources } = require('webpack');
+const webpackMajorVersion = Number(require('webpack/package.json').version.split('.')[0]);
+
+class WebpackCleanCommentsPlugin {
+
+  constructor(opts) {
+    this.options = opts || {};
+    this.options.test = this.options.test || /\.js($|\?)/i;
+    if (!this.options.comments) this.options.comments = [/^\/\*[A-Za-z0-9\s*():/.",\-!_$@#%&~]+\*\/\n?/];
+    if (!Array.isArray(this.options.comments)) this.options.comments = [this.options.comments];
+  }
+
+  apply(compiler) {
+    const { webpack } = compiler;
+    if (webpackMajorVersion < 5) {
+      compiler.hooks.emit.tapAsync('WebpackCleanCommentsPlugin',
+      /**
+       * Hook into the webpack emit phase
+       * @param {WebpackCompilation} compilation
+       * @param {(err?: Error) => void} callback
+      */
+        (compilation, callback) => {
+          this.processAssets(compilation.assets).filter(Boolean).forEach(
+            ({ filename, source }) => {
+              compilation.assets[filename] = {
+                source: () => source,
+                size: () => source.length,
+              };
+            },
+          );
+          callback();
+        });
+    } else {
+      compiler.hooks.thisCompilation.tap('WebpackCleanCommentsPlugin',
+        compilation => {
+          compilation.hooks.processAssets.tapAsync(
+            {
+              name: 'WebpackCleanCommentsPlugin',
+
+              stage: webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE,
+            },
+            (compilationAssets, callback) => {
+              // eslint-disable-next-line no-console
+              console.log('compilationAssets', compilationAssets);
+              this.processAssets(compilation.assets).filter(Boolean).forEach(
+                ({ filename, source }) => {
+                  compilation.updateAsset(filename, source);
+                },
+              );
+              callback();
+            },
+          );
+        });
+    }
+  }
+
+  processAssets(assets) {
+    return Object.keys(assets)
+      .filter(filename => this.testFileName(filename))
+      .map(filename => {
+        const asset = assets[filename];
+        const source = asset.source();
+        const { comments } = this.options;
+        let newSource = source;
+        comments.forEach(comment => newSource = newSource.replace(comment, ''));
+        if (source.length === newSource.length) return;
+        return {
+          filename,
+          source: new sources.RawSource(newSource),
+        };
+      });
+  }
+
+  testFileName(filename) {
+    if (this.options.test instanceof RegExp) {
+      return this.options.test.test(filename);
+    }
+
+    if (typeof this.options.test === 'string') {
+      return this.options.test === filename;
+    }
+
+    if (typeof this.options.test === 'function') {
+      return this.options.test(filename);
+    }
+
+    throw new Error(
+      // eslint-disable-next-line max-len
+      '[webpack-clean-comments-plugin]: Invalid "test" option. May be a RegExp (tested against asset key), a string containing the key, a function(key): bool',
+    );
+  }
+
+}
+
+module.exports = WebpackCleanCommentsPlugin;
